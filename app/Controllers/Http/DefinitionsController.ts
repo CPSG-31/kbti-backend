c176d634a5b713712765817f8cb04f27bd1cd40b
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-27 23:31:13 +0700

    refactor: response status, constant, and conditional term

--- app/Controllers/Http/DefinitionsController.ts
+++ app/Controllers/Http/DefinitionsController.ts
@@ -6,13 +6,50 @@ import { DateTime } from 'luxon'
 
 export default class DefinitionsController {
   public async index({ params, response }: HttpContextContract) {
+    const STATUS_DEFINITION_APPROVED = 2
+
     const { term } = params
+    const formattedTerm = decodeURI(term).trim()
+    if (!formattedTerm) {
+      return response.status(422).send({
+        code: 422,
+        status: 'Error',
+        message: 'Term is required',
+      })
+    }
+
+    if (formattedTerm === '*') {
+      const definitions = await Definition.query()
+        .preload('user')
+        .preload('category')
+        .where('status_definition_id', STATUS_DEFINITION_APPROVED)
+        .orderBy('updated_at', 'desc')
+
+      return response.status(200).json({
+        code: 200,
+        status: 'Success',
+        message: 'Definitions found',
+        data: definitions.map((data) => {
+          const { id, term, definition, user, category, createdAt, updatedAt } = data
+
+          return {
+            id,
+            term,
+            definition,
+            category: category.category,
+            username: user.username,
+            created_at: getUnixTimestamp(createdAt),
+            updated_at: getUnixTimestamp(updatedAt),
+          }
+        }),
+      })
+    }
     try {
       const definitions = await Definition.query()
         .preload('user')
         .preload('category')
-        .where('status_definition_id', 2)
-        .where('term', 'like', `${term}`)
+        .where('status_definition_id', STATUS_DEFINITION_APPROVED)
+        .where('term', 'like', `%${formattedTerm}%`)
 
       if (definitions.length === 0) {
         return response.status(404).json({
@@ -22,7 +59,7 @@ export default class DefinitionsController {
         })
       }
 
-      return response.json({
+      return response.status(200).json({
         code: 200,
         status: 'Success',
         message: 'Definitions found',
@@ -40,7 +77,7 @@ export default class DefinitionsController {
         }),
       })
     } catch (error) {
-      return response.json({
+      return response.status(500).json({
         code: 500,
         status: 'Error',
         message: 'Internal server error',
@@ -57,13 +94,12 @@ export default class DefinitionsController {
         ...payload,
         statusDefinitionId: DEFAULT_STATUS_DEFINITION_ID,
       }
-      const newDefinition = await Definition.create(validData)
+      await Definition.create(validData)
 
       return response.status(201).json({
         code: 200,
         status: 'Success',
         message: 'Definition created',
-        data: newDefinition,
       })
     } catch (error) {
       if (error.name === 'ValidationException') {
@@ -89,7 +125,7 @@ export default class DefinitionsController {
 
       const definition = await Definition.findOrFail(id)
       if (!definition) {
-        return response.json({
+        return response.status(404).json({
           code: 404,
           status: 'Not Found',
           message: 'Definition not found',
@@ -104,11 +140,10 @@ export default class DefinitionsController {
       definition.merge(validData)
       await definition.save()
 
-      return response.json({
+      return response.status(200).json({
         code: 200,
         status: 'Success',
         message: 'Definition updated',
-        data: definition,
       })
     } catch (error) {
       if (error.name === 'ValidationException') {
@@ -128,27 +163,28 @@ export default class DefinitionsController {
 
   public async destroy({ params, response }: HttpContextContract) {
     const { id } = params
+    const STATUS_DEFINITION_DELETED = 4
     try {
       const definition = await Definition.findByOrFail('id', id)
       if (!definition) {
-        return response.json({
+        return response.status(404).json({
           code: 404,
           status: 'Not Found',
           message: 'Definition not found',
         })
       }
 
-      definition.statusDefinitionId = 4
+      definition.statusDefinitionId = STATUS_DEFINITION_DELETED
       definition.deletedAt = DateTime.local()
       await definition.save()
 
-      return response.json({
+      return response.status(200).json({
         code: 200,
         status: 'Success',
         message: 'Definition deleted',
       })
     } catch (error) {
-      return response.json({
+      return response.status(500).json({
         code: 500,
         status: 'Error',
         message: 'Internal server error',
