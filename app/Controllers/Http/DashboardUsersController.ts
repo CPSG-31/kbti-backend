a7abdf3f74b34900b1c63c24cc4ac594445bdbc0
Tryo Asnafi <tryoasnafi@gmail.com>
2022-06-04 02:11:08 +0700

    refactor: add variable type and improve response DashboardUsersController

--- app/Controllers/Http/DashboardUsersController.ts
+++ app/Controllers/Http/DashboardUsersController.ts
@@ -1,7 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
-import { getUnixTimestamp } from 'App/Helpers/Customs'
+import { createResponse, getUnixTimestamp } from 'App/Helpers/Customs'
 import Definition from 'App/Models/Definition'
+import User from 'App/Models/User'
 
 export default class DashboardUsersController {
   private STATUS_DEFINITION_REVIEW: number = 1
@@ -9,17 +10,20 @@ export default class DashboardUsersController {
   private STATUS_DEFINITION_REJECTED: number = 3
   private STATUS_DEFINITION_DELETED: number = 4
 
+  protected res: ResponseInterface = createResponse({ code: 200, status: 'Success' })
+
   public async index({ response, auth }: HttpContextContract): Promise<void> {
-    const { id: userId, username, email } = auth.user!
+    const { id: userId, username, email }: User = auth.user!
+
     try {
-      const definitions = await Definition.query()
+      const definitions: Definition[] = await Definition.query()
         .preload('category')
         .preload('statusDefinition')
         .where('user_id', userId)
         .whereNot('status_definition_id', this.STATUS_DEFINITION_DELETED)
         .orderBy('updated_at', 'desc')
 
-      const dataDefinitions = definitions.map((data) => {
+      const dataDefinitions: Object[] = definitions.map((data) => {
         const { id, term, definition, category, statusDefinition, createdAt, updatedAt } = data
         return {
           id,
@@ -32,44 +36,42 @@ export default class DashboardUsersController {
         }
       })
 
-      const totaldefinitionApproved = await this.getTotalDefinition(
+      const totaldefinitionApproved: any = await this.getTotalDefinition(
         userId,
         this.STATUS_DEFINITION_APPROVED
       )
 
-      const totaldefinitionReview = await this.getTotalDefinition(
+      const totaldefinitionReview: any = await this.getTotalDefinition(
         userId,
         this.STATUS_DEFINITION_REVIEW
       )
 
-      const totaldefinitionRejected = await this.getTotalDefinition(
+      const totaldefinitionRejected: any = await this.getTotalDefinition(
         userId,
         this.STATUS_DEFINITION_REJECTED
       )
 
-      return response.status(200).json({
-        code: 200,
-        status: 'Success',
-        data: {
-          userId,
-          username,
-          email,
-          total_approved: totaldefinitionApproved.total,
-          total_review: totaldefinitionReview.total,
-          total_reject: totaldefinitionRejected.total,
-          definitions: dataDefinitions,
-        },
-      })
-    } catch (error) {
-      return response.status(500).json({
-        code: 500,
-        status: 'Error',
-        message: 'Internal server error',
-      })
+      this.res.data = {
+        userId,
+        username,
+        email,
+        total_approved: totaldefinitionApproved?.total || 0,
+        total_review: totaldefinitionReview?.total || 0,
+        total_reject: totaldefinitionRejected?.total || 0,
+        definitions: dataDefinitions,
+      }
+
+      return response.status(this.res.code).json(this.res)
+    } catch (error: any) {
+      this.res.code = 500
+      this.res.status = 'Error'
+      this.res.message = 'Internal server error'
+
+      return response.status(this.res.code).json(this.res)
     }
   }
 
-  private async getTotalDefinition(userId: number, statusId: number) {
+  private async getTotalDefinition(userId: number, statusId: number): Promise<any> {
     return Database.from('definitions')
       .count('* as total')
       .where('user_id', userId)
