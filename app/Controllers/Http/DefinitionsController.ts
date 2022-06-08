59015220ada5862a475af8c69f4e5c032614d7c6
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-06-08 20:30:26 +0700

    refactor: move function storeVote to DefinitionsController

--- app/Controllers/Http/DefinitionsController.ts
+++ app/Controllers/Http/DefinitionsController.ts
@@ -5,6 +5,8 @@ import User from 'App/Models/User'
 import CreateDefinitionValidator from 'App/Validators/CreateDefinitionValidator'
 import { DateTime } from 'luxon'
 import StatusDefinitions from 'App/Enums/StatusDefinitions'
+import VoteValidator from 'App/Validators/VoteValidator'
+import Vote from 'App/Models/Vote'
 
 export default class DefinitionsController {
   protected res: ResponseInterface = createResponse({ code: 200, status: 'Success' })
@@ -260,5 +262,51 @@ export default class DefinitionsController {
       .where('id', id)
       .whereNot('status_definition_id', StatusDefinitions.DELETED)
       .firstOrFail()
+  }
+
+  public async storeVote({ params, response, request, auth }: HttpContextContract): Promise<void> {
+    const { id: definitionId }: Record<string, any> = params
+    const { id: userId }: { id: number } = auth.user!
+
+    try {
+      const { is_upvote: isUpvote }: { is_upvote: boolean } = await request.validate(VoteValidator)
+
+      await Definition.findOrFail(definitionId)
+
+      await Vote.updateOrCreate(
+        {
+          userId,
+          definitionId,
+        },
+        {
+          userId,
+          definitionId,
+          isUpvote,
+        }
+      )
+
+      this.res.code = 201
+      this.res.message = 'Definition voted'
+
+      return response.status(this.res.code).json(this.res)
+    } catch (error: any) {
+      this.res.code = 500
+      this.res.status = 'Error'
+      this.res.message = 'Internal Server Error'
+
+      if (error.code === 'E_VALIDATION_FAILURE') {
+        this.res.code = 422
+        this.res.status = 'Validation Error'
+        this.res.message = error.messages
+      }
+
+      if (error.code === 'E_ROW_NOT_FOUND') {
+        this.res.code = 404
+        this.res.status = 'Not Found'
+        this.res.message = 'Definition not found'
+      }
+
+      return response.status(this.res.code).json(this.res)
+    }
   }
 }
