ee021bdb84b71425c0dcbbf2e83a5179be56b03e
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-06-09 10:57:09 +0700

    refactor: table catefgorie name to unique and update seeder category

--- database/migrations/1653530578242_categories.ts
+++ database/migrations/1653530578242_categories.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('category', 80)
+      table.string('category', 80).unique()
     })
   }
 
