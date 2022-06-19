559c58605e5b1798e5a176a43e3d1889a470145c
Tryo Asnafi <tryoasnafi@gmail.com>
2022-06-19 20:59:58 +0700

    feat: add get token info

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,6 +1,6 @@
 import { OpaqueTokenContract } from '@ioc:Adonis/Addons/Auth'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { createResponse } from 'App/Helpers/Customs'
+import { createResponse, getUnixTimestamp } from 'App/Helpers/Customs'
 import User from 'App/Models/User'
 import CreateUser from 'App/Validators/CreateUserValidator'
 import Roles from 'App/Enums/Roles'
@@ -16,16 +16,21 @@ export default class AuthController {
       }
 
       const user: User = await User.findByOrFail('email', email)
-      const token: OpaqueTokenContract<User> = await auth.use('api').attempt(email, password, {
+      const apiToken: OpaqueTokenContract<User> = await auth.use('api').attempt(email, password, {
         expiresIn: '2hours',
       })
+      const { type, token, expiresAt } = apiToken
 
       this.res.data = {
         user_id: user?.id,
         role_id: user?.roleId || Roles.USER,
         username: user?.username,
         email: email,
-        access_token: token,
+        access_token: {
+          type: type.toLowerCase(),
+          token: token,
+          expires_at: getUnixTimestamp(expiresAt!),
+        },
       }
 
       return response.status(this.res.code).json(this.res)
@@ -82,7 +87,7 @@ export default class AuthController {
 
   public async logout({ auth, response }: HttpContextContract): Promise<void> {
     try {
-      await auth.use('api').revoke()
+      await auth.use('api').logout()
       this.res.message = 'User has been logged out'
 
       return response.status(this.res.code).json(this.res)
@@ -90,6 +95,42 @@ export default class AuthController {
       this.res.code = 500
       this.res.status = 'Error'
       this.res.message = 'Internal Server Error'
+
+      return response.status(this.res.code).json(this.res)
+    }
+  }
+
+  public async getTokenInfo({ request, response, auth }: HttpContextContract): Promise<void> {
+    try {
+      const authorization = request.headers().authorization!
+      const [authType, authToken] = authorization.split(' ')
+
+      const user = auth.use('api').user!
+      const { expiresAt } = auth.use('api').token!
+
+      this.res.data = {
+        user_id: user.id,
+        role_id: user.roleId,
+        username: user.username,
+        email: user.email,
+        access_token: {
+          type: authType.toLowerCase(),
+          token: authToken,
+          expires_at: getUnixTimestamp(expiresAt!),
+        },
+      }
+
+      return response.status(this.res.code).json(this.res)
+    } catch (error: any) {
+      this.res.code = 500
+      this.res.status = 'Error'
+      this.res.message = 'Internal Server Error'
+
+      if (error.code === 'E_ROW_NOT_FOUND') {
+        this.res.code = 404
+        this.res.status = 'Not Found'
+        this.res.message = 'Data not found'
+      }
 
       return response.status(this.res.code).json(this.res)
     }
